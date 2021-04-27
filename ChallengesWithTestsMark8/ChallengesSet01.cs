0d50bc2be9bd700119d46c7bb0fc8e9c2ea07fb2
Zezuz <mczanaty92@gmail.com>
2021-04-26 23:16:00 -0500

    Adding updated week 1,2,3

--- ChallengesWithTestsMark8/ChallengesSet01.cs
+++ ChallengesWithTestsMark8/ChallengesSet01.cs
@@ -18,31 +18,50 @@ namespace ChallengesWithTestsMark8
 
         public double Subtract(double minuend, double subtrahend)
         {
-            throw new NotImplementedException();
+            return minuend - subtrahend;
         }
 
         public int Add(int number1, int number2)
         {
-            throw new NotImplementedException();
+           return number1 + number2;
         }
 
         public int GetSmallestNumber(int number1, int number2)
         {
+            if (number1 > number2)
+            {
+                return number2;
+            }
+            else
+            {
+                return number1;
+            }
             throw new NotImplementedException();
+
+
         }
 
         public long Multiply(long factor1, long factor2)
         {
-            throw new NotImplementedException();
+            return (factor1 * factor2);
         }
 
-        public string GetGreeting(string nameOfPerson)
+        public string GetGreeting(string nameofperson)
         {
+            if (nameofperson == "")
+            {
+                return "Hello!";
+            }
+            else
+            {
+                return $"Hello, {nameofperson}!";
+            }
             throw new NotImplementedException();
         }
 
         public string GetHey()
         {
+            return "HEY!";
             throw new NotImplementedException();
         }
     }
--- ChallengesWithTestsMark8/ChallengesSet03.cs
+++ ChallengesWithTestsMark8/ChallengesSet03.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
@@ -7,47 +8,124 @@ namespace ChallengesWithTestsMark8
     {
         public bool ArrayContainsAFalse(bool[] vals)
         {
-            throw new NotImplementedException();
+
+            var arr = vals.Contains(false);
+            return arr;
         }
 
         public bool IsSumOfOddsOdd(IEnumerable<int> numbers)
         {
-            throw new NotImplementedException();
+            var sum = 0;
+            if (numbers == null)
+            {
+                return false;
+            }
+            
+            foreach (var num in numbers)
+            {
+                if (num % 2 != 0)
+                {
+                    sum += num;
+                }
+                
+            }
+            if (sum % 2 != 0)
+            {
+                return true;
+            }
+            return false;
+
         }
 
         public bool PasswordContainsUpperLowerAndNumber(string password)
         {
-            throw new NotImplementedException();
+            //var isLower = false;
+            //var isUpper = false;
+            //var isNumber = false;
+
+            //for (int i = 0; i < password.Length; i++)
+            //{
+            //    if (char.IsLower(password[i])) 
+            //    {
+            //        isLower = true;
+            //    }
+            //    if (char.IsUpper(password[i]))
+            //    {
+            //        isUpper = true;
+            //    }
+            //    if (char.IsNumber(password[i]))
+            //    {
+            //        isNumber = true;
+            //    }
+            //}
+
+            //if (isLower && isUpper && isNumber)
+            //{
+            //    return true;
+            //}
+            //else 
+            //{
+            //    return false;
+            //}
+
+            return password.Any(char.IsNumber);
+                   password.Any(char.IsUpper);
+                   password.Any(char.IsLower);
         }
 
         public char GetFirstLetterOfString(string val)
         {
-            throw new NotImplementedException();
+            char c = val.FirstOrDefault();
+            return c;
+
+            //return val[0]; a string is just an array and it can be accessed via brackets
         }
 
         public char GetLastLetterOfString(string val)
         {
-            throw new NotImplementedException();
+            char c = val.Last();
+            return c;
         }
 
         public decimal Divide(decimal dividend, decimal divisor)
         {
-            throw new NotImplementedException();
+            return divisor == 0 ? 0 : dividend / divisor;
+
+            //if (divisor == 0)
+            // return 0;
+            //else
+            // return dividend/divisor;
+
         }
 
         public int LastMinusFirst(int[] nums)
         {
-            throw new NotImplementedException();
+            return nums.Last() - nums.First();
         }
 
         public int[] GetOddsBelow100()
         {
-            throw new NotImplementedException();
+            int oddIncrement = 1;
+            int[] x = new int[50];
+            for (int i = 0; i < 50; i++)
+            {
+                x[i] = oddIncrement;
+                oddIncrement += 2;
+            }
+            return x;
         }
 
         public void ChangeAllElementsToUppercase(string[] words)
         {
-            throw new NotImplementedException();
+            foreach (var word in words)
+            {
+                word.ToUpper();
+            }
+
+            //for (int i = 0; i < words.Length; i++)
+            //{
+            //    words[i] = words[i].ToUpper();
+            //}
         }
     }
 }
